e04374d0b4be00ecb8b329c2720eed60d52a995b
Ben Thompson <benjamin.philip.thompson@maine.edu>
2021-01-13 18:13:00 -0500

    fix build since app.py is in the same dir

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask,render_template, jsonify,request,Response
 
 from flask_cors import CORS
 
-app = Flask(__name__, static_folder='../build', static_url_path='/')
+app = Flask(__name__, static_folder='./build', static_url_path='/')
 app.debug = False
 CORS(app)
 
